fd907350ef302666246b55e6eceffb05045a00c9
Klim Trotsenko <klim.trotsenko7@gmail.com>
2023-10-25 19:06:30 +0300

    add homework

---
+++ tests/android_app/conftest.py
@@ -0,0 +1,46 @@
+import pytest
+from appium.options.android import UiAutomator2Options
+from selene import browser
+import os
+from dotenv import load_dotenv
+
+
+
+
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management():
+    load_dotenv()
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "android",
+        "platformVersion": "9.0",
+        "deviceName": "Google Pixel 3",
+
+        # Set URL of the application under test
+        "app": "bs://sample.app",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "First Python project",
+            "buildName": "browserstack-build-1",
+            "sessionName": "BStack first_test",
+
+            # Set your access credentials
+            "userName": os.getenv('BS_USERNAME'),
+            "accessKey": os.getenv('BS_PASSWORD')
+        }
+    })
+
+    # Initialize the remote Webdriver using BrowserStack remote URL
+    # and options defined above
+    # browser.config.driver = webdriver.Remote("http://hub.browserstack.com/wd/hub", options=options)
+    # либо этот сверху либо две строчки снизу вариант передачи драйвера
+    browser.config.driver_remote_url = "http://hub.browserstack.com/wd/hub"
+    browser.config.driver_options = options
+    # селен верхние две строчки сам преобразуем в верхнюю одну
+
+    browser.config.timeout = float(os.getenv('timeout', '10.0'))
+    yield
+
+    browser.quit()
---
+++ tests/web/conftest.py
@@ -0,0 +1,35 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attach
+
+@pytest.fixture(scope='function', autouse=True)
+def setup_browser(request):
+    browser_version = "100.0"
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser.config.driver = driver
+    browser.config.base_url = 'https://www.wikipedia.org'
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+    browser.quit()
